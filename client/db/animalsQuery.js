e8c4ddad4e756bdf478aa72043c181669f94026a
julia franzoi <julia_franzoi@hotmail.com>
2017-02-21 22:40:17 +0000

    page with nameTab

---
+++ client/db/animalsQuery.js
@@ -0,0 +1,9 @@
+//builds a query for the database
+var MongoClient = require('mongodb').MongoClient;
+var FilmQuery = function(){
+  this.url = 'mongodb://localhost:27017/ratings_site';
+};
+
+
+
+module.exports = animalsRouter;
